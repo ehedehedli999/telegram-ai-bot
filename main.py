7d5671000052012765981d4dbd44549a2996f8e8
ehedehedli999 <146145398+ehedehedli999@users.noreply.github.com>
2026-01-22 00:26:05 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,8 @@ import telebot
 import google.generativeai as genai
 import os
 
-# BURAYA DOKUNMA! Bu isimler Hugging Face Settings'teki isimlerle aynı olmalı.
-TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
-GEMINI_KEY = os.environ.get('GEMINI_KEY')
-
-# Eğer şifreler gelmezse hata mesajı ver
-if not TELEGRAM_TOKEN or not GEMINI_KEY:
-    print("HATA: Ayarlar (Secrets) kısmında isimler yanlış yazılmış!")
-    exit(1)
+TELEGRAM_TOKEN = os.environ.get('8333602244:AAHU0BsG3pyyQ-RFt8FVFK-ih8IPCuCzvHw')
+GEMINI_KEY = os.environ.get('AIzaSyBB1H7YC2D6bC7oNImuGuMq7elV5w49wp4')
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 genai.configure(api_key=GEMINI_KEY)
@@ -19,7 +13,6 @@ model = genai.GenerativeModel('gemini-pro')
 def handle_message(message):
     try:
         bot_username = bot.get_me().username
-        # Özel mesaj veya grupta etiketlenme durumu
         if message.chat.type == "private" or f"@{bot_username}" in message.text:
             prompt = message.text.replace(f"@{bot_username}", "").strip()
             if prompt:
@@ -28,5 +21,5 @@ def handle_message(message):
     except Exception as e:
         print(f"Hata: {e}")
 
-print("Bot başarıyla başlatıldı!")
+print("Bot çalışıyor...")
 bot.polling(non_stop=True)
